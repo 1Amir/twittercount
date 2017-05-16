fba99bb4436e49cf7d8aad3543417608e3f83347
amir computing software <amirandshamar@gmail.com>
2017-05-15 20:06:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-#code has not been tested as I have not been able to download the import tweepy.
+#Errors are still in the code
 import json
 import tweepy
 from tweepy import OAuthHandler
-auth = OAuthHandler(https://twitter.com/realDonaldTrump)  
+ 
+apiad = ('https://twitter.com/realDonaldTrump')
+ 
+auth = OAuthHandler(apiad)
+ 
 api = tweepy.API(auth)
 for tweet in tweepy.Cursor(api.user_timeline).items():
     process_or_store(tweet._json)
