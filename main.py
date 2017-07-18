46220ef3033a981cea3a4d4eed06a578ca43653d
amir computing software <amirandshamar@gmail.com>
2017-07-18 11:21:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 callbackurl = 'https://twitter.com/settings/devices?success=true'
-consumer_key = 'NYQJgVDpiJJZ92bGeFJ3QWopg'
-consumer_secret = 'EJ6cPG4Itr1jk3gn4raIBwZdXvK0afS65fC5VK5V53pTdocXgt'
-access_token = '865405999041044481-GMDNMBwY4zKAgR2mY7xGNAUqFcH2Tdh'
-access_secret = 'D5zp2ZCst0bRNjHAxvypRb7CHTuks9iNedz5TF4vwzR9L'
+consumer_key = ''
+consumer_secret = ''
+access_token = '8h'
+access_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret,callbackurl)
 auth.set_access_token(access_token, access_secret)
